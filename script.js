f032ab8577f13bf2a30409caa3fab7581f2429bc
NAVEENS K <knaveens408@gmail.com>
2025-12-30 22:43:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.getElementById("resume-btn").addEventListener("click", (e) => {
     e.preventDefault();
 
-    const resumeUrl = "https://naveens-resume-bucket.s3.ap-south-1.amazonaws.com/resume.pdf";
+    const resumeUrl = "https://s3-for-my-resume.s3.ap-south-1.amazonaws.com/NAVEENS_K%5B1%5D.pdf";
 
     window.open(resumeUrl, "_blank");
 });
