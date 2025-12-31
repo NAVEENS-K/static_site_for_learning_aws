c5e36146d8a8fa833c785f3bfc0c999004ce7e84
NAVEENS K <knaveens408@gmail.com>
2025-12-31 09:55:03 +0530

    Redirect to resume page on button click

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
 document.getElementById("resume-btn").addEventListener("click", (e) => {
     e.preventDefault();
-
-    const resumeUrl = "https://s3-for-my-resume.s3.ap-south-1.amazonaws.com/NAVEENS_K%5B1%5D.pdf";
-
-    window.open(resumeUrl, "_blank");
+    window.location.href = "/resume";
 });
